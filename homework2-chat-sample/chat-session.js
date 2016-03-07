195c9a23598483012f5371379d4b545efd5f2203
Jennifer <jrodr757@fiu.edu>
2016-03-06 21:10:49 -0500

    a

--- homework2-chat-sample/chat-session.js
+++
@@ -1,8 +0,0 @@
-/**
- * Chat Session Data is stored here.
- */
-module.exports = {
-	players: [],
-	count: 0,
-	log: ""
-}
--- homework2-chat-sample/readme.md
+++
@@ -1,3 +0,0 @@
-# Chat App w/ Sockets & Express
-
-A simple chat app built with sockets and express.
